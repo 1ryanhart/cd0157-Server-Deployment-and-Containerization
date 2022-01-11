4ae9394d9a13786ebd3d4b895900e13fad4f5bb7
Ryan-Hart <1ryanhart@gmail.com>
2022-01-11 13:03:49 +0000

    added assert False to test build failure

--- test_main.py
+++ test_main.py
@@ -17,7 +17,7 @@ def client():
     os.environ['JWT_SECRET'] = SECRET
     main.APP.config['TESTING'] = True
     client = main.APP.test_client()
-
+    assert False
     yield client
 
 
@@ -31,7 +31,7 @@ def test_health(client):
 def test_auth(client):
     body = {'email': EMAIL,
             'password': PASSWORD}
-    response = client.post('/auth', 
+    response = client.post('/auth',
                            data=json.dumps(body),
                            content_type='application/json')
 
